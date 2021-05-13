2089ac50e9c6447da4685e8eb7141ac698556f4f
Paul Richardson <developer@photographerafoot.com>
2021-05-14 07:44:20 +1000

    Initial Commit

---
+++ CapitalCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Paul Richardson on 14/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
