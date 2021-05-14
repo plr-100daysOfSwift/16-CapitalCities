c95f24283941d7a65ea816a9a7bb699b1c4703d3
Paul Richardson <developer@photographerafoot.com>
2021-05-15 09:04:48 +1000

    CHG: prompt the user to choose a map type
    add navigation bar button
    present an alert

--- CapitalCities/ViewController.swift
+++ CapitalCities/ViewController.swift
@@ -11,9 +11,11 @@ import MapKit
 class ViewController: UIViewController, MKMapViewDelegate {
 
 	@IBOutlet var mapView: MKMapView!
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Map Type", style: .plain, target: self, action: #selector(changeMapType))
 
 		let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
 		let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
@@ -22,6 +24,20 @@ class ViewController: UIViewController, MKMapViewDelegate {
 		let washington = Capital(title: "Washington", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
 
 		mapView.addAnnotations([london, oslo, paris, rome, washington])
+	}
+
+	@objc func changeMapType() {
+		let ac = UIAlertController(title: "Choose a map type", message: nil, preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "Standard", style: .default, handler: { _ in
+			self.mapView.mapType = .standard
+		}))
+		ac.addAction(UIAlertAction(title: "Satellite", style: .default, handler: { _ in
+			self.mapView.mapType = .satellite
+		}))
+		ac.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: { _ in
+			self.mapView.mapType = .hybrid
+		}))
+		present(ac, animated: true)
 	}
 
 	// MARK:- MapView Delegate Methods
