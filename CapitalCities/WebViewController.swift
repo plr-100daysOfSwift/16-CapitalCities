e78ca7815d13aaec263d6beeb0391b1d0162c7d1
Paul Richardson <developer@photographerafoot.com>
2021-05-15 10:00:42 +1000

    CHG: show error if URL is invalid

--- CapitalCities/WebViewController.swift
+++ CapitalCities/WebViewController.swift
@@ -23,9 +23,26 @@ class WebViewController: UIViewController {
 		guard let city = city else { return }
 		let urlString = "http://en.wikipedia.org/?search=\(city)"
 		if let myURL = URL(string: urlString) {
-			let myRequest = URLRequest(url: myURL) 
+			let myRequest = URLRequest(url: myURL)
 			webView.load(myRequest)
+		} else {
+			showError(url: urlString)
 		}
 	}
 
+	func showError(url: String) {
+		let html = """
+			<html>
+			<head>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<style>body { margin: 20px } div { font-size: 120% }</style>
+			</head>
+			<body>
+			<h1>An error occurred!</h1>
+			<div>"\(url)" is not a valid URL</div>
+			</body>
+			</html>
+			"""
+		webView.loadHTMLString(html, baseURL: nil)
+	}
 }
